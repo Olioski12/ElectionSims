f39ff6e89915b10dcdba38399c94140f3ffc1fc1
Oliver Papillo <oliverpapillo2@gmail.com>
2023-09-30 11:38:24 +1000

    Gallagher added

--- ENP.R
+++ ENP.R
@@ -3,7 +3,8 @@
 #model seats
 #swing and 
 
-percs <- c(0.75,0.25)
+percs <- c(0.3947,0.3189,0.1971,0.0466,0.0345,0.0082)
+seat_percs <- c(0.5444,0.2929,0.1302,0.0296,0.0029,0)
 
 #effective number of parties
 ENP <- function(vote_shares){
@@ -14,6 +15,17 @@ ENP <- function(vote_shares){
   }
   
   return(1/sum)
+}
+
+Gallagher <- function(vote_shares, seat_shares){
+  sum <- 0 
+  for (n in 1:length(vote_shares)){
+    share = vote_shares[n]*100;
+    seat = seat_shares[n]*100;
+    sum <- sum+(seat-share)^2
+  }
+  
+  return(sqrt(sum/2))
 }
 
 # ENPG <- function(vote_shares){
@@ -27,3 +39,4 @@ ENP <- function(vote_shares){
 # }
 
 ENP(percs)
+Gallagher(percs,seat_percs)
