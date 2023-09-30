47f82622c1e0bcd5d4adcfc344fac21b37b5ac33
Oliver Papillo <oliverpapillo2@gmail.com>
2023-09-30 11:43:42 +1000

    Added normalisation

--- ENP.R
+++ ENP.R
@@ -3,11 +3,13 @@
 #model seats
 #swing and 
 
-percs <- c(0.3947,0.3189,0.1971,0.0466,0.0345,0.0082)
+percs <- c(3947,3189,1971,466,345,82)
 seat_percs <- c(0.5444,0.2929,0.1302,0.0296,0.0029,0)
 
 #effective number of parties
 ENP <- function(vote_shares){
+  tsum = sum(vote_shares);
+  vote_shares <- vote_shares/tsum
   sum <- 0 
   for (n in 1:length(vote_shares)){
     share = vote_shares[n]
@@ -18,10 +20,14 @@ ENP <- function(vote_shares){
 }
 
 Gallagher <- function(vote_shares, seat_shares){
+  tsum = sum(vote_shares);
+  vote_shares <- vote_shares/tsum*100
+  tsum2 = sum(seat_shares);
+  seat_shares <- seat_shares/tsum2*100
   sum <- 0 
   for (n in 1:length(vote_shares)){
-    share = vote_shares[n]*100;
-    seat = seat_shares[n]*100;
+    share = vote_shares[n];
+    seat = seat_shares[n];
     sum <- sum+(seat-share)^2
   }
   
